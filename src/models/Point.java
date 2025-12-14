26647cafbcbcf4b6ae540fc427c0e0db75e0d454
YerassylAmangazy <252757@astanait.edu.kz>
2025-12-14 19:01:04 +0600

    Update Point.java

--- src/models/Point.java
+++ src/models/Point.java
@@ -4,9 +4,22 @@ public class Point {
     private double x;
     private double y;
 
-    // constructor having both parameters
+    // constructor
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-    // distance(Point dest) - distance from current point to the dest point
+    // distance from current point to destination point
+    public double distance(Point dest) {
+        return Math.sqrt(
+                Math.pow(dest.x - this.x, 2) +
+                Math.pow(dest.y - this.y, 2)
+        );
+    }
 
-    // toString
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
